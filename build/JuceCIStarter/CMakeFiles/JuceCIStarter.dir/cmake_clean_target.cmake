7a48e64e9b529d9f6f221573bbe32bf220b4c643
colton8dio <111544382+colton8dio@users.noreply.github.com>
2022-11-30 09:56:31 -0500

    Delete build directory

--- build/JuceCIStarter/CMakeFiles/JuceCIStarter.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "JuceCIStarter_artefacts/libJUCE CI Starter_SharedCode.a"
-)
--- build/JuceCIStarter/CMakeFiles/JuceCIStarter_AU.dir/__/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_Unity.cpp.o.d
+++
@@ -1,9 +0,0 @@
-JuceCIStarter/CMakeFiles/JuceCIStarter_AU.dir/__/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_Unity.cpp.o: \
-  /Volumes/iMacPro_Ext/Local\ Storage/8Dio/JUCE_CI/juce_ci_starter/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_Unity.cpp \
-  /Volumes/iMacPro_Ext/Local\ Storage/8Dio/JUCE_CI/juce_ci_starter/JUCE/modules/juce_audio_plugin_client/Unity/juce_Unity_Wrapper.cpp \
-  /Volumes/iMacPro_Ext/Local\ Storage/8Dio/JUCE_CI/juce_ci_starter/JUCE/modules/juce_core/system/juce_TargetPlatform.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX13.0.sdk/usr/include/TargetConditionals.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX13.0.sdk/usr/include/AvailabilityMacros.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX13.0.sdk/usr/include/Availability.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX13.0.sdk/usr/include/AvailabilityVersions.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX13.0.sdk/usr/include/AvailabilityInternal.h
